571158b0a4c4a04f330bfc89b6bec4d9699e60be
Christian Heinrich <christian.heinrich@livando.com>
2012-08-29 12:11:55 -0500

    Updated install.sh script.
    Should setup everything nicely now. Maybe we should
    integrate vim-support here, too

--- install.sh
+++ install.sh
@@ -6,12 +6,18 @@ typeset -A filenames
 #key = filename in repo
 #value = filename as seen from $HOME.
 
-filenames=(.screenrc .screenrc .Xdefaults .Xdefaults .Xmodmap .Xmodmap "bin/xmonad.start" "bin/xmonad.start" "zsh/zshrc" .zshrc)
+if [ ! -d $HOME/bin ]
+  then
+     mkdir $HOME/bin
+fi
+
+filenames=(.screenrc .screenrc .gitconfig .gitconfig .Xdefaults .Xdefaults .Xmodmap .Xmodmap "xmonad/bin/xmonad.start" "bin/xmonad.start" "xmonad/" ".xmonad" "xmonad/xmobarrc" ".xmobarrc" "zsh/zshrc" .zshrc)
 for file in ${(k)filenames}
 do
     if [ ! -e $HOME/$filenames[$file] ]
     then
         ln -s $PWD/$file $HOME/$filenames[$file]
+	echo "$HOME/$filenames[$file] - created." 
     else
         echo "$HOME/$filenames[$file] exists - skipping."
     fi
