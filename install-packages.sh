401a606d05a8b42cb2952db5c84216495fefab2b
Christian Heinrich <christian.heinrich@livando.com>
2012-08-29 12:09:37 -0500

    Added install-packages.sh
    This file will help you setup your new system.

---
+++ install-packages.sh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+
+typeset -A packages
+# suckless-tools = dmenu
+packages=(xmonad 'xmonad xmobar suckless-tools trayer' screen "screen" "rxvt-unicode-256color" "rxvt-unicode-256color" git "git" "evolution" "evolution" keepassx keepassx )
+
+for package in ${(k)packages}
+do
+    # This actually does not work if package was installed before
+    # and then removed... Should consider something else!
+    dpkg -s $package > /dev/null &> /dev/null
+    if [ $? -gt 0 ]
+      then
+        echo -n "$package is not installed. Do you want to install $package? [y/N]" 
+        read install
+
+        if [ $install = "y" ]
+          then
+            # Install xmonad tiling window manager
+            sudo apt-get install ${(z)packages[$package]}
+          else
+            echo -n "Skipping $package.\n"
+        fi
+    fi
+    if [[ $? -eq 0 || $install -eq "y" ]]
+      then
+        # setup dotfiles here for $package ...
+    fi
+done
