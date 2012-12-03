dac018123ba57320fb6c8e40d21fa95a07ad3e1f
Christian Heinrich <christian.heinrich@livando.com>
2012-12-03 00:05:57 -0600

    Installscript now sets up .xsession script.
    However, make sure you read the README file within the xmonad/ directory

--- install.sh
+++ install.sh
@@ -11,13 +11,13 @@ if [ ! -d $HOME/bin ]
      mkdir $HOME/bin
 fi
 
-filenames=(.screenrc .screenrc .gitconfig .gitconfig .Xdefaults .Xdefaults .Xmodmap .Xmodmap "xmonad/bin/xmonad.start" "bin/xmonad.start" "xmonad/" ".xmonad" "xmonad/xmobarrc" ".xmobarrc" "zsh/zshrc" .zshrc)
+filenames=(.screenrc .screenrc .gitconfig .gitconfig .Xdefaults .Xdefaults .Xmodmap .Xmodmap "xmonad/xsession" ".xsession" "xmonad/" ".xmonad" "xmonad/xmobarrc" ".xmobarrc" "zsh/zshrc" .zshrc)
 for file in ${(k)filenames}
 do
     if [ ! -e $HOME/$filenames[$file] ]
     then
         ln -s $PWD/$file $HOME/$filenames[$file]
-	echo "$HOME/$filenames[$file] - created." 
+	echo "$HOME/$filenames[$file] - created."
     else
         echo "$HOME/$filenames[$file] exists - skipping."
     fi
