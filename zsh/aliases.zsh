3d566035c2c1bdf171a550ede0419ea3e526ab88
Christian Heinrich <christian.heinrich@livando.com>
2015-05-25 23:14:58 +0200

    If colordiff is installed, ZSH will bind diff to it.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -24,6 +24,8 @@ then
   alias acsh='apt-cache show'
 fi
 
+[[ -x $(which colordiff) ]] && alias diff="colordiff -u" || alias diff="diff -u"
+
 alias lessc=/var/lib/gems/1.8/bin/lessc
 
 # Global aliases for often used commands in the command line.
