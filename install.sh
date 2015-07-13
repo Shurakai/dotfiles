e40729d2e16da502c0766ac986be503924d5f211
Christian Heinrich <christian.heinrich@livando.com>
2015-07-13 19:27:19 +0200

    Added pentadactylrc to this repository

--- install.sh
+++ install.sh
@@ -11,10 +11,10 @@ if [ ! -d $HOME/bin ]
      mkdir $HOME/bin
 fi
 
-filenames=(.cgvgrc .cgvgrc gpg.conf ../.gnupg/gpg.conf "screen/screenrc" .screenrc .gitconfig .gitconfig "most/.mostrc" ".mostrc" .Xresources .Xresources .Xmodmap .Xmodmap "xmonad/xsession" ".xsession" "xmonad/" ".xmonad" "xmonad/xmobarrc" ".xmobarrc" "zsh/zshrc" .zshrc "zsh/zshenv" .zshenv)
+filenames=(.cgvgrc .cgvgrc gpg.conf ../.gnupg/gpg.conf "pentadactyl/pentadactylrc" .pentadactylrc "screen/screenrc" .screenrc .gitconfig .gitconfig "most/.mostrc" ".mostrc" .Xresources .Xresources .Xmodmap .Xmodmap "xmonad/xsession" ".xsession" "xmonad/" ".xmonad" "xmonad/xmobarrc" ".xmobarrc" "zsh/zshrc" .zshrc "zsh/zshenv" .zshenv)
 for file in ${(k)filenames}
 do
-    if [ ! -e $HOME/$filenames[$file] ]
+    if [[ ! -e $HOME/$filenames[$file] ]]
     then
         ln -s $PWD/$file $HOME/$filenames[$file]
 	echo "$HOME/$filenames[$file] - created."
